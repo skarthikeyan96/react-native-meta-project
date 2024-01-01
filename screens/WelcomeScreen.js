ba1bf2aa7f3884651c2157ab61c5ddd878ddc259
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-01-01 18:09:50 +0530

    feat: replace npm with yarn

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { View, Text, Button } from "react-native";
+import tw from 'twrnc'
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{margin: "10px"}}>
+      <Text style={tw`text-md text-black dark:text-white`}>Open up App.js to start working on your app!</Text>
       <Button
         title="Go to Profile"
         onPress={() => navigation.navigate('Subscribe')}
